e46da09388e900257b57e4d4b296bf60d6a7a445
Ricardo Cid <rcid@myring.io>
2015-09-26 06:02:01 -0400

    Create editdistance.py

---
+++ editdistance.py
@@ -0,0 +1,30 @@
+#this calculates edit distance not levenstein edit distance
+class editdistance:
+
+    def ed(self,word1,word2):
+
+        len_1=len(word1)
+
+        len_2=len(word2)
+
+        x =[[0]*(len_2+1) for _ in range(len_1+1)]#the matrix whose last element ->edit distance
+
+        for i in range(0,len_1+1): #initialization of base case values
+
+            x[i][0]=i
+        for j in range(0,len_2+1):
+
+            x[0][j]=j
+        for i in range (1,len_1+1):
+
+            for j in range(1,len_2+1):
+
+                if word1[i-1]==word2[j-1]:
+                    x[i][j] = x[i-1][j-1] 
+
+                else :
+                    x[i][j]= min(x[i][j-1],x[i-1][j],x[i-1][j-1])+1
+
+        return x[i][j]
+
+
